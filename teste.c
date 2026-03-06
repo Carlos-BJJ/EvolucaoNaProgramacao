dddb6804777f3708f74bd529cdb091d246399f42
Carlos-BJJ <carlos.pereira21ofc@gmail.com>
2026-03-06 20:02:58 -0300

    .

---
+++ teste.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+unsigned long int tamanho ;
+unsigned long long int tamanho2x = 4000000000;
+
+printf("%lu bytes \n", sizeof(tamanho));
+printf("%lu bytes \n", sizeof(tamanho2x));
+
+unsigned long int tamanho3 = 4000000000;
+
+printf("O número de habitantes da cidade: %lu ", tamanho3);
+
+}
