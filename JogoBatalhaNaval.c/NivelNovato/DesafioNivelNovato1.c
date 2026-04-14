b7423af656b4e8e67b6d447305835fe175f6c3c0
Carlos-BJJ <carlos.pereira21ofc@gmail.com>
2026-04-14 20:18:02 -0300

    DesafioNivelNovato1 Vetores e matrizes feito

--- JogoBatalhaNaval.c/NivelNovato/DesafioNivelNovato1.c
+++ JogoBatalhaNaval.c/NivelNovato/DesafioNivelNovato1.c
@@ -2,31 +2,40 @@
 
 int main(){
 
-int tabuleiro[10][10] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-};
+//Tabuleiro iniciado 
+int tabuleiro[10][10] = {0};
 
-int navio1 [3] = {3, 3, 3}, navio2 [3] = {3, 3, 3};
+//Definindo os navios
+int NavioHorizontal [3] = {3, 3, 3}, NavioVertical [3] = {3, 3, 3};
 
-int cont = 0, cont2 = 0;
+//Definindo as coordenadas
+int linhaH = 2, colunaH = 1; // Navio Horizontal
+int linhaV = 5, colunaV = 7; // Navio Vertical
 
-for (cont; cont <= 9; cont++){
-    printf("%d ", tabuleiro[cont][cont]);
+for(int i = 0; i < 3; i++){
+    tabuleiro[linhaH][colunaH + i] = NavioHorizontal[i];
+} 
+/*
+Eu somo a coluna com i, pois eu quero que os valores estejam
+um do lado do outro
+*/
 
-    for (cont2; cont2 <= 8; cont2++){
-        printf("%d ", tabuleiro[0][cont2]);
+for(int i = 0; i < 3; i++){
+    tabuleiro[linhaV + i][colunaV] = NavioVertical[i];
 }
-    
-printf("\n");
+/*
+Eu somo a linha com i, pois eu quero que os valores estejam
+um embaixo do outro
+*/
+
+printf("=== TABULEIRO BATALHA NAVAL ===\n\n");
+for (int i = 0; i < 10; i++){
+    for (int j = 0; j < 10; j++){
+        printf("%d ", tabuleiro[i][j]);
+}
+    printf("\n");
 }
 
-}
+
+
+} 
